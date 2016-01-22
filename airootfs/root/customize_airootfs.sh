8af3d777db06da2c94734289c2868c4ab5711717
Chris Harvey <chris@chrisnharvey.com>
2016-01-22 20:30:08 +0000

    Enable gdm plymouth

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -20,5 +20,5 @@ sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
-systemctl enable gdm.service
+systemctl enable gdm-plymouth.service
 systemctl enable NetworkManager.service
